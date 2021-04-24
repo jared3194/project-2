7298719eb2b14715e2d86e56c97d62d0f92c59ca
znurtokatli <50844314+znurtokatli@users.noreply.github.com>
2021-04-24 11:02:35 -0400

    db-objects

--- src/db-objects.sql
+++ src/db-objects.sql
@@ -1,18 +1,41 @@
+ create database aviation
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8';
+ 
+--create tables under aviation schema 
+ 
 create table airports (
-	airport_id
-	airport_name
-	iata
-	icao
-	timezone
-	 );
+	airport_id serial not null primary key,
+	airport varchar(1000) not null,
+	iata varchar(100) not null,
+	icao varchar(100) not null,
+	timezone varchar(100),
+	constraint airports_uk unique (iata, icao) );
+ 
+create table airlines (
+	airline_id serial not null primary key,
+	airline varchar(1000) not null,
+	iata varchar(100) not null,
+	icao varchar(100) not null, 
+	constraint airlines_uk unique (iata, icao) );	
 
-
-
-
-create table airlines
-
-
-
-create table flights
-
-
+create table flights (
+	flight_id serial not null primary key,
+	flight_number varchar(500) not null,
+	flight_type varchar(100) not null,
+	iata varchar(100) not null,
+	icao varchar(100) not null,
+	airport_id int not null,
+	airline_id int not null,
+	terminal varchar(100),
+	gate varchar(100),
+	baggage varchar(100),
+	delay varchar(100),
+	scheduled date,--varchar(100),
+	estimated date,
+	actual date, 
+	estimated_runway date,
+	actual_runway date,
+	constraint flights_fk1 foreign key(airport_id) references airports(airport_id),	 
+	constraint flights_fk2 foreign key(airline_id) references airlines(airline_id) );	 	
