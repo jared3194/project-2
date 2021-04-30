bb50888df0b8dbdd3e6d049d2ad92eb5c591ef9a
Melinda <melindaeudy@gmail.com>
2021-04-30 08:01:30 -0400

    consolidated work folders

--- aviation/py/models.py
+++ aviation/py/models.py
@@ -17,12 +17,21 @@ with engine.connect() as connection:
     airlines_table = Table('airlines', metadata, autoload=True, autoload_with=engine)
 
 class airports(Base):
-    _table_ = Base.metadata.tables['airports']
+    __table__ = Base.metadata.tables['airports']
 
-    def _repr_(self):
-        return '''<airlines(airport_id='{0}', airport='{1}', iata='{2}', icao='{3}', timezone='{4}'>'''.format(self.airport_id, self.airport, self.iata, self.icao, self.timezone)
+    def __repr__(self):
+        return '''<airports(airport_id='{0}', airport='{1}', iata='{2}', icao='{3}', timezone='{4}'>'''.format(self.airport_id, self.airport, self.iata, self.icao, self.timezone)
 
-if _name_ == "_base_":
+class airlines(Base):
+    __table__ = Base.metadata.tables['airlines']
+
+    def __repr__(self):
+        return '''<airlines(airline_id='{0})', airline='{1}', iata='{2}', icao='{3}'>'''.format(self.airline_id, self.airline, self.iata, self.icao)
+
+class flights(Base):
+    
+
+if __name__ == "__base__":
     with engine.connect() as connection:
         connection.execute("COMMIT")
         connection.execute('CALL airports')
