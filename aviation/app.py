3e4941f7515ecd7c1005a3f0b0c463ec26a4dc68
Melinda <melindaeudy@gmail.com>
2021-04-30 10:59:26 -0400

    changed file names

--- aviation/app.py
+++ aviation/app.py
@@ -1,16 +1,41 @@
-from flask import render_template, request, redirect, Blueprint
+from flask import Flask
 import os.path
 from config import conn_str
+from flask_sqlalchemy import SQLAlchemy
+import sqlalchemy
+from sqlalchemy.sql import func
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy import engine
+from sqlalchemy import create_engine, func, MetaData, Table
+from flask import Flask, jsonify
+from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy.ext.declarative import declarative_base
 
-bp = Blueprint('../templates/views', __name__)
+
+DATABASE_URI = conn_str
+engine = create_engine(DATABASE_URI)
+Base = declarative_base()
+
+# bp = Blueprint('app.py', __name__)
 
 app = Flask(__name__)
 app.config['DATABASE_URL'] = conn_str
-app.config.update(SECRET_KEY=b'C\xcd\x15t\xaf-\xa3\x80`\xf1!u\xbaBE\x03K\x9d\xdf\xc1\x90H\xf2\xf2/')
+app.config['SECRET_KEY'] ='C\xcd\x15t\xaf-\xa3\x80`\xf1!u\xbaBE\x03K\x9d\xdf\xc1\x90H\xf2\xf2/'
+
+db = SQLAlchemy(app)
 
 
-@bp.route('/')
-def base():
-    return render_template("../base.html")
+    
+Base.metadata.create_all(engine)
+
+def create_session():
+    session = sessionmaker(bind=engine)
+    return session()
+
+@app.route('/')
+def Base():
+    return ("base.html")
 
 app.run(debug=True, host="127.0.0.1", port=5000)
+
+    
--- aviation/models.py
+++ aviation/models.py
@@ -1,51 +1,34 @@
-import sqlalchemy
-from sqlalchemy.sql import func
-from sqlalchemy.ext.automap import automap_base
-from sqlalchemy import engine
-from sqlalchemy import create_engine, func, MetaData, Table
-from flask import Flask, jsonify
-from sqlalchemy.orm import sessionmaker, relationship
-from sqlalchemy.ext.declarative import declarative_base
-from config import conn_str
+from app import db
 
-DATABASE_URI = conn_str
-engine = create_engine('DATABASE_URI', isolation_level="AUTOCOMMIT")
-Base = declarative_base()
-Base.metadata = reflect()
-
-# with engine.connect() as connection:
-#     meta = MetaData(engine)
-#     airlines_table = Table('airlines', metadata, autoload=True, autoload_with=engine)
-
-class airports(Base):
+class airports(db.Model):
     __tablename__ = 'airports'
 
-    airport_id = Column(Integer, primary_key=True)
-    airport = Column(String(length=50))
-    iata = Column(String(length=5))
-    icao = Column(String(length=5))
-    timezone = Column(String(length = 25))
+    airport_id = db.Column(db.Integer, primary_key=True)
+    airport = db.Column(db.String(length=50))
+    iata = db.Column(db.String(length=5))
+    icao = db.Column(db.String(length=5))
+    timezone = db.Column(db.String(length = 25))
 
-    def __repr__(self):
+    def __init__(self):
         return '''<airports(airport_id='{0}', airport='{1}', iata='{2}', icao='{3}', timezone='{4}')>'''.format(self.airport_id, self.airport, self.iata, self.icao, self.timezone)
 
 class airlines(Base):
     __tablename__ = 'airlines'
 
-    airlines_id = Column(Integer, primary_key=True)
-    airline = Column(String(length=50))
-    iata = Column(String(length=5))
-    icao = Column(String(length=5))
+    airlines_id = db.Column(db.Integer, primary_key=True)
+    airline = db.Column(db.String(length=50))
+    iata = db.Column(db.String(length=5))
+    icao = db.Column(db.String(length=5))
 
-    def __repr__(self):
+    def __init__(self):
         return '''<airlines(airline_id='{0})', airline='{1}', iata='{2}', icao='{3}')>'''.format(self.airline_id, self.airline, self.iata, self.icao)
 
 class flights(Base):
     __tablename__ = 'flights'
 
-    flight_id = Column(Integer, primary_key=True)
-    flight_number = Column(String(length=5))
-    flight_type = Column(String(length=15))
+    flight_id = db.Column(db.Integer, primary_key=True)
+    flight_number = db.Column(db.String(length=5))
+    flight_type = db.Column(db.String(length=15))
     iata = Column(String(length=5))
     icao = Column(String(length=5))
     airport_id = Column(String(length=5))
@@ -59,11 +42,11 @@ class flights(Base):
     estimated_runway = Column(String(length = 15))
     actual_runway = Column(String(length = 15))
 
-    def __repr__(self):
+    def __init__(self):
         return '''<flights(flight_id='{0}', flight_number='{1}', flight_type='{2}', iata='{3}', icao='{4}', airport_id='{5}', terminal='{6}', gate='{7}', baggage='{8}', delay='{9}', scheduled='{10}', estimated='{11}', actual='{12}', estimated='{13}', estimated_runway='{14}', actual_runway='{15}')>'''.format(self.flight_id, self.flight_number, self.flight_type, self.iata, self.icao, self.airline_id, self.terminal, self.gate, self.baggage, self.delay, self.scheduled, self.estimated, self.actual, self.estimated_runway, self.actual_runway)
 
-    Base.metadata.create_all(engine)
+if __name__ == "__main__":
 
-    def create_session():
-        session = sessionmaker(bind=engine)
-        return session()
+    print ("Creating database tables...")
+    db.create_all()
+    
