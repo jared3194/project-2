f2e284039f285bb46a64bca00d7bd4f871cb3e84
Jared Nieves <jared3194@gmail.com>
2021-05-01 12:13:04 -0400

    pushing changes

--- src/__init__.py
+++ src/__init__.py
@@ -6,25 +6,28 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func, Table
 from sqlalchemy.sql.schema import MetaData
+import pandas as pd
 
-engine = create_engine("postgres://postgres:c1W9d9Cx$@localhost:5432/aviation")
-metadata = MetaData()
-metadata.reflect(engine, only = ['airlines', 'airports', 'flights'])
-Base = automap_base(metadata = metadata)
-Base.prepare()
+
 
 # print(Base.metadata.tables)
-Airlines = Base.classes.airlines
-print(Airlines)
+
 
 def create_app():
     app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'adsfjkahdfkjlahoiuwncvejiasnd'
     # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:c1W9d9Cx$@localhost:5432/aviation'
     # db = SQLAlchemy(app)
+    engine = create_engine(
+    "postgres://postgres:c1W9d9Cx$@localhost:5432/aviation")
+    flight_details = pd.read_sql_query('SELECT * FROM flight_details', engine.connect())
+    for rows in flight_details:
+        print(rows) 
+    
     from .views import views
 
     app.register_blueprint(views, url_prefix = '/')
-    # from .models import airlines
+   
     
 
     return app
