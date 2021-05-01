49d2da346df4edd2fb2307bbf23ff54ace0b5e26
znurtokatli <50844314+znurtokatli@users.noreply.github.com>
2021-05-01 13:27:53 -0400

    dep-iata

--- src/db-queries2.sql
+++ src/db-queries2.sql
@@ -1,46 +1,14 @@
- create database aviation
-    WITH 
-    OWNER = postgres
-    ENCODING = 'UTF8';
- 
---create tables under aviation schema 
- 
-create table airports (
-	airport_id serial not null primary key,
-	airport varchar(1000) not null,
-	iata varchar(100) not null,
-	icao varchar(100) not null,
-	timezone varchar(100),
-	constraint airports_uk unique (iata, icao) );
- 
-create table airlines (
-	airline_id serial not null primary key,
-	airline varchar(1000) not null,
-	iata varchar(100) not null,
-	icao varchar(100) not null, 
-	constraint airlines_uk unique (iata, icao) );	
+--delete from flights
 
-create table flights (
-	flight_id serial not null primary key,
-	flight_number varchar(500) not null,
-	flight_type varchar(100) not null,
-	iata varchar(100) not null,
-	icao varchar(100) not null,
-	airport_id int not null, 
-	terminal varchar(100),
-	gate varchar(100),
-	baggage varchar(100),
-	delay varchar(100),
-	scheduled date,--varchar(100),
-	estimated date,
-	actual date, 
-	estimated_runway date,
-	actual_runway date
-);	 	 
+select * from flight_details where flight_number = '2023'
    
-create  view flight_details
-as
-select split_part(dpf.flight_number, '_', 2)  as flight_number,
+select  split_part(flight_number, '_', 2)
+--substring(flight_number from '_#%' for '#'  ) as flight_number
+--regexp_substr
+from flights where flight_number like  '%2023%' --2023
+
+select dpf.flight_number,  
+	   split_part(dpf.flight_number, '_', 2) , 
 	   dpf.iata flight_iata,
 	   dpf.icao flight_icao,
 	   dpf.airport_id departure_airport_id,
@@ -79,7 +47,6 @@ select split_part(dpf.flight_number, '_', 2)  as flight_number,
    and dpf.flight_type = 'DEPARTURE'  
    and arf.airport_id = ara.airport_id
    and arf.flight_type = 'ARRIVAL'
-   and dpf.flight_number = arf.flight_number ;
+   and dpf.flight_number = arf.flight_number 
+   and split_part(dpf.flight_number, '_', 2) like  '%2023%'
  
- 
-
