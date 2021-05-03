051064859888c9407111af011a4488958841c7bc
unknown <sarahmgreen88@gmail.com>
2021-05-03 11:22:26 -0400

    updating logic

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,16 +1,24 @@
 var count = ('country_count.csv')
-d3.csv(count, function(data) {
-  createFeatures(data.features);
-});
-function createFeatures(countryData) {
-  var arrival = (countryData, {
-    onEachFeature: function(feature,layer){
-      layer.bindPopup("<h1>Airport: "+ feature.Arrive + 
-      "<h1>Number of Flights:  " + feature.Count )
-  }})}
-  createMap(arrival);
+// d3.csv(count).then(function (data) {
+//   console.log(data)
+//   for (var i = 0; i < data.length; i++) {
+//   Markers = []
+//   Markers.push(
+//     L.marker(data[i].Lat, data[i].Long), {icon: icons}.bindPopup("<h1>Airport: " + data.Arrive +
+//       "<h1>Number of Flights:  " + data.Count).addTo(myMap)
+  // createMap(arrival)
+
+// function createFeatures(countryData) {
+//   var arrival = (countryData, {
+//     onEachFeature: function (feature, layer) {
+//       layer.bindPopup("<h1>Airport: " + feature.Arrive +
+//         "<h1>Number of Flights:  " + feature.Count)
+//     }
+//   })
+
+
 function createMap(arrival) {
-  var lightmap =L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     tileSize: 512,
     maxZoom: 18,
@@ -24,14 +32,9 @@ function createMap(arrival) {
     LAX: new L.LayerGroup(),
     BOTH: new L.LayerGroup(),
   };
-  
-  var overlays = {
-    "Atlanta": layers.ATL,
-    "Los Angeles": layers.LAX,
-    "Both": layers.BOTH,
 
-  };
-  var myMap = L.map("map", {
+
+  var myMap = L.map("map_id", {
     center: [40.7, -94.5],
     zoom: 5,
     layers: [
@@ -40,49 +43,84 @@ function createMap(arrival) {
       layers.BOTH,
     ]
   });
-lightmap.addTo(map);
 
-// Creating map object
-var myMap = L.map("map", {
-  center: [40.7, -94.5],
-  zoom: 5,
-  layers: [
-    layers.ATL,
-    layers.LAX,
-    layers.BOTH,
+  // Creating map object
+  // var myMap = L.map("map", {
+  //   center: [40.7, -94.5],
+  //   zoom: 5,
+  //   layers: [
+  //     layers.ATL,
+  //     layers.LAX,
+  //     layers.BOTH
+  //   ]
+  // });
+  lightmap.addTo(myMap);
 
-  ]
+  var overlays = {
+    "Atlanta": layers.ATL,
+    "Los Angeles": layers.LAX,
+    "Both": layers.BOTH,
+    "All": arrival
+  };
+  L.control.layers(null, overlays).addTo(myMap);
 
-});
-L.control.layers(null, overlays).addTo(myMap);
-
-var legend = L.control({
-  position: "bottomright"
-});
-function getColorGrades(Airport) {
-  if (Airport = "LAX") {
-      return 'blue'
-  } else if (Airport = "ATL") {
-      return 'red'
-  } else if (Airport = "BOTH") {
-      return 'purple'
-  }
+  var icons = {
+  ATL: L.ExtraMarkers.icon({
+    icon: "ion-android-airplane",
+    iconColor: "white",
+    markerColor: "red",
+    shape: "star"
+  }),
+  LAX: L.ExtraMarkers.icon({
+    icon: "ion-android-airplane",
+    iconColor: "white",
+    markerColor: "blue",
+    shape: "circle"
+  }),
+  BOTH: L.ExtraMarkers.icon({
+    icon: "ion-android-airplane",
+    iconColor: "white",
+    markerColor: "purple",
+    shape: "penta"
+  })
 };
 
-legend.onAdd = function(myMap) {
-  var div = L.DomUtil.create("div", "info legend"),
-  Airport = ["LAX", "ATL", "BOTH"]
-  div.innerHTML += "<h3 style='text-align: center'>Airport</h3>"
+  var legend = L.control({
+    position: "bottomright"
+  });
+  function getColorGrades(Airport) {
+    if (Airport = "LAX") {
+      return 'blue'
+    } else if (Airport = "ATL") {
+      return 'red'
+    } else if (Airport = "BOTH") {
+      return 'purple'
+    }
+  };
 
-for (var i = 0; i <Airport.length; i++) {
-  div.innerHTML +=
-  '<i style="background:' + getColorGrades(Airport[i] + 1) + '"></i> ' +
-  Airport[i] + (Airport[i + 1] ? '&ndash;' + Airport[i + 1] + '<br>' : '+');
-}
+  legend.onAdd = function (myMap) {
+    var div = L.DomUtil.create("div", "info legend"),
+      Airport = ["LAX", "ATL", "BOTH"]
+    div.innerHTML += "<h3 style='text-align: center'>Airport</h3>"
+
+    for (var i = 0; i < Airport.length; i++) {
+      div.innerHTML +=
+        '<i style="background:' + getColorGrades(Airport[i] + 1) + '"></i> ' +
+        Airport[i] + (Airport[i + 1] ? '&ndash;' + Airport[i + 1] + '<br>' : '+');
+    }
     return div;
-}
+  }
   legend.addTo(myMap);
 }
+d3.csv(count).then(function (data) {
+  console.log(data)
+  for (var i = 0; i < data.length; i++) {
+  Markers = []
+  Markers.push(
+    L.marker(data[i].Lat, data[i].Long), {icon: icons}.bindPopup("<h1>Airport: " + data.Arrive +
+      "<h1>Number of Flights:  " + data.Count).addTo(myMap)
+      );
+    }});
 // // Adding tile layer
 // L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
 //   attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
